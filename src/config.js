4b4300413df75d86d04dd1d926de354fc01c133a
Malik39 <usama.faisal@daoproptech.com>
2023-11-19 03:21:52 +0500

    updated config file

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   port: process.env.PORT || 3000,
-  mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/yourdb',
-  redisUrl: process.env.REDIS_URL || 'redis://localhost:6379',
+  mongodbUri: process.env.MONGODB_URI || 'mongodb+srv://mfaisalusama:zjasUMI2RHCp3Fta@cluster0.1hxkj4a.mongodb.net/Codistan',
+  redisUrl: process.env.REDIS_URL || 'redis://128.199.135.131:6379',
 };
