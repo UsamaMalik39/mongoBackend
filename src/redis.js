959480e95e301d333c73058e22b354c17dc6855f
Malik39 <usama.faisal@daoproptech.com>
2023-11-19 02:29:42 +0500

    update variables for redis

--- src/redis.js
+++ src/redis.js
@@ -1,6 +1,26 @@
 const redis = require('redis');
 const { redisUrl } = require('./config');
+const axios = require('axios');
+const fs = require('fs');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const updateIpAddress = async () => {
+  try {
+    const response = await axios.get('https://ifconfig.me/ip');
+    const ipAddress = response.data.trim();
+
+    process.env.REDIS_URL = 'redis://'+ipAddress+':6379';
+    fs.writeFileSync('.env', `REDIS_URL=${process.env.REDIS_URL}\n`);
+    console.log('Server IP Address updated:', ipAddress);
+  } catch (error) {
+    console.error('Error updating IP address:', error.message);
+  }
+};
+
+
+updateIpAddress();
 const client = redis.createClient(redisUrl);
 
 client.on('error', (err) => {
